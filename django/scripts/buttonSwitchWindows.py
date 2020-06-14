98e39570d53b303e94dcb7020b468ed8449ea03b
Stefan Landvogt <stefan.landvogt@pentlandfirth.com>
2020-06-14 11:20:29 +0200

    fix time

--- django/scripts/buttonSwitchWindows.py
+++ django/scripts/buttonSwitchWindows.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python2.7
 # script by Alex Eames http://RasPi.tv/
 # http://raspi.tv/2013/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio
-import RPi.GPIO as GPIO
 import time
-import urllib2
 from datetime import datetime
 #from myconfig import *
+from keyboard import press
 import requests
-GPIO.setmode(GPIO.BCM)
 
 code = '666'
 url = 'https://firestore.googleapis.com/v1/projects/kickcam-57681/databases/(default)/documents/buttonClicks'
@@ -27,7 +25,6 @@ def push():
 	return True
 #
 
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 print ("Waiting for Button Input...")
 # now the program will do nothing until the signal on port 23
@@ -37,15 +34,6 @@ print ("Waiting for Button Input...")
 #print "During this waiting time, your computer is not"
 #print "wasting resources by polling for a button press.\n"
 #print "Press your button when ready to initiate a falling edge interrupt."
-while True:
-	try:
-		GPIO.wait_for_edge(23, GPIO.RISING)
-		print ("\nButton Press detected. Firing POST Call to:\n")
-		print(url)
-		push(url)
-		time.sleep(0.3)
-	except KeyboardInterrupt:
-		GPIO.cleanup()       # clean up GPIO on CTRL+C exit
-		raise
-	#GPIO.cleanup()           # clean up GPIO on normal exit
-
+#txt = input("Type something to test this out: ")
+print (data)
+push()
